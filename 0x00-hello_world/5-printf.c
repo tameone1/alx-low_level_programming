dd04d7b95223ffc26e3373cfa63219f4d28c1e47
tameone1 <assefa.tameru3@gmail.com>
2023-02-17 08:31:32 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is piece of art, \n");
+	return(0);
+}
